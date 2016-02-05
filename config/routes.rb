5fe4f85fe23a0f01f555fca8a3e90773ea0de6d1
Technology Department <technology@Kehs-iMac-29298.local>
2016-02-05 09:46:57 -0500

    chapter 7_register

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+ 
   root 'home#about'
 
-  get 'home/help'
+  get 'help' => 'home#help'
+
+  get 'login' => 'home#login'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
